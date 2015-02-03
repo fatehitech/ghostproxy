13077c9f64b2de661a5eb6ef29ee8d4f1cf63f42
Keyvan Fatehi <keyvanfatehi@gmail.com>
2015-02-03 11:31:32 +0200

    stub ghost reaper tests

---
+++ test/ghost_reaper_test.js
@@ -0,0 +1,17 @@
+var expect = require('chai').expect;
+var GhostReaper = require('../src/ghost_reaper');
+
+describe("GhostReaper::reap", function() {
+  it("turns off the machine", function() {
+    
+  });
+  it("snapshots the machine on digitalocean", function() {
+    
+  })
+  it("deletes the previous snapshot from digitalocean", function() {
+    
+  });
+  it("replaces the previous snapshot id", function() {
+    
+  });
+})
