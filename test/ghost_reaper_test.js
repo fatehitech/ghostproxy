2080441f19872bb28ef1cc3adae6aa4befef58ac
Keyvan Fatehi <keyvanfatehi@gmail.com>
2015-02-04 20:40:19 +0200

    add ghostreaper test

--- test/ghost_reaper_test.js
+++ test/ghost_reaper_test.js
@@ -1,8 +1,16 @@
 var expect = require('chai').expect;
 var GhostReaper = require('../src/ghost_reaper');
 
+var ghost = null;
+
+beforeEach(function() {
+  ghost = {};
+});
+
 describe("GhostReaper::reap", function() {
   it("turns off the machine", function() {
+
+    GhostReaper.reap(ghost)
     
   });
   it("snapshots the machine on digitalocean", function() {
