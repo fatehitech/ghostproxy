31693e6a9de64c2a3ec2642f17f765e10751aeaf
Keyvan Fatehi <keyvanfatehi@gmail.com>
2015-02-04 16:11:26 +0200

    use Ghosts for worker stuff

--- src/ghost_activator.js
+++ src/ghost_activator.js
@@ -1,8 +1,5 @@
 module.exports = GhostActivator;
 var logger = require('./logger');
-var config = require('../etc/config');
-var monq = require('monq');
-var client = monq(config.mongoURI);
 var VPS = require('./vps');
 var Ghosts = require('./ghosts');
 
@@ -14,48 +11,31 @@ GhostActivator.prototype.requestActivation = function(done) {
   if (this.ghost.status >= Ghosts.WAITING) return done();
   var ghost = this.ghost;
   Ghosts.updateStatus(this.ghost, Ghosts.WAITING).then(function() {
-    var queue = client.queue('vps');
-    queue.enqueue('create', { ghost: ghost }, function (err, job) {
-      if (err) return logger.error(err.stack);
-      logger.info('enqueued vps::create');
-    });
+    Ghosts.enqueueJob(ghost, 'vps', 'create');
     done();
   }, done);
 }
 
 GhostActivator.work = function() {
-  logger.info('init GhostActivator')
-  var worker = client.worker(['vps']);
-  worker.register({
-    create: function (params, callback) {
-      var vps = new VPS(params.ghost);
-      try {
-        logger.info('vps::creator start');
-        vps.create().then(function() {
-          logger.info("Created VPS");
-          return vps.start().then(function() {
-            logger.info("Started VPS");
-            callback(null);
-          });
-        }, function(err) {
-          logger.error(err.stack);
-          callback(err);
+  logger.info('init GhostActivator');
+  Ghosts.createWorker('vps', 'create', function (ghost, params, callback) {
+    console.log('Vps create', ghost);
+    var vps = new VPS(ghost);
+    console.log(vps);
+    return;
+    try {
+      vps.create().then(function() {
+        logger.info("Created VPS");
+        return vps.start().then(function() {
+          logger.info("Started VPS");
+          callback(null);
         });
-      } catch (e) {
-        callback(e);
-      }
+      }, function(err) {
+        logger.error(err.stack);
+        callback(err);
+      });
+    } catch (e) {
+      callback(e);
     }
-  });
-
-  worker.on('dequeued', function (data) { });
-  worker.on('failed', function (data) {
-    logger.error(data.stack);
-  });
-  worker.on('complete', function (data) {
-    logger.info('job complete');
-  });
-  worker.on('error', function (err) {
-    logger.error(err.stack);
-  });
-  worker.start();
+  }).start();
 }
